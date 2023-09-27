0f5d1516161b0241259ec050d226cd516ed62680
ValeryBurlakov <wogl@mail.ru>
2023-09-28 01:37:24 +0300

    finally a working handler 'add_photo'

--- main.py
+++ main.py
@@ -7,8 +7,10 @@ from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
 from aiogram.dispatcher import FSMContext
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher.filters.builtin import CommandStart
 import mysql.connector
 from config import TOKEN2, PASSWORD, HOST, USER, DATABASE
+
 # import json
 
 # 4. Создаем соединение с базой данных в вашем файле Python:
@@ -29,8 +31,8 @@ cursor = mydb.cursor()
 
 
 # 6. Создаем команду `/start`, чтобы бот мог приветствовать пользователей и добавлять их в базу данных:
-
-@dp.message_handler(commands=['start'])
+# также можно использовать: commands=['start']
+@dp.message_handler(CommandStart())
 async def start(message: types.Message):
     user_id = message.from_user.id
     username = message.from_user.username
@@ -73,25 +75,25 @@ async def process_coin_name(message: types.Message, state: FSMContext):
     mydb.commit()
     await message.answer(f'{coin_name}\n  Введите год монеты:')
     # await state.finish()
-    await state.set_state("waiting_year") # переход в следующее состояние
-
+    await state.set_state("waiting_year")  # переход в следующее состояние
 
 
 @dp.message_handler(state="waiting_year")
 async def process_coin_name(message: types.Message, state: FSMContext):
     coin_year = message.text  # Получаем year монеты из команды
 
-    cursor.execute("SELECT MAX(id) FROM coin") # получили id нашей новой монеты из базы
+    cursor.execute("SELECT MAX(id) FROM coin")  # получили id нашей новой монеты из базы
     id_our_coin = cursor.fetchone()
     cursor.execute("UPDATE coin SET _year = %s WHERE id = %s", (coin_year, id_our_coin[0]))
     # 0 индекс так как результат fetchone это множество
-    mydb.commit() # сохранили изменения
+    mydb.commit()  # сохранили изменения
     cursor.execute("SELECT _description FROM coin WHERE id = %s", id_our_coin)  # получили id нашей новой монеты из базы
     coin_name = cursor.fetchone()
 
-    await state.finish() # закончили с добавлением 
+    await state.finish()  # закончили с добавлением
 
     await message.answer(f"Монета {coin_name[0]} добавлена в коллекцию.")
+
 
 # ===================================================================
 
@@ -118,40 +120,34 @@ async def process_coin_name(message: types.Message, state: FSMContext):
 
     await state.finish()
     await message.answer(f"коллекция {collection_name} добавлена к юзеру @{username}.")
+
+
 # ===============================================================
+@dp.message_handler(commands=['add_photo'])
+async def add_coin(message: types.Message, state: FSMContext):
+    await message.answer(f'пришлите фото')
+    await state.set_state("test_add_photo")  # переход в следующее состояние
 
 
-# @dp.message_handler(state="waiting_for_coin_photo", content_types=types.ContentType.PHOTO)
-# async def process_coin_photo(message: types.Message, state: FSMContext):
-#     user_id = message.from_user.id
-#     coin_name = (await state.get_data())['coin_name']
-#     photo_id = message.photo[-1].file_id  # Get the ID of the last (largest) photo sent by the user
-#
-#     # Save the photo ID in the photos table associated with the coin
-#     cursor.execute("INSERT INTO photos (coin_id, photo_id) VALUES (NULL, %s)",
-#                    (photo_id,))
-#     coin_id = cursor.lastrowid
-#     mydb.commit()
-#
-#     # Add the coin to the coin collection
-#     cursor.execute("SELECT coin_collection FROM users WHERE id = %s", (user_id,))
-#     result = cursor.fetchone()
-#
-#     if result is not None:
-#         coin_collection = result[0]
-#     else:
-#         coin_collection = ","
-#
-#     if coin_collection is not None:
-#         coin_collection += coin_name + ","
-#     else:
-#         coin_collection = coin_name + ","
-#
-#     cursor.execute("UPDATE users SET coin_collection = %s WHERE id = %s", (coin_collection, user_id))
-#     mydb.commit()
-#
-#     await state.finish()
-#     await message.answer(f"Монета {coin_name} добавлена в коллекцию.")
+@dp.message_handler(state="test_add_photo", content_types=types.ContentType.PHOTO)
+async def process_photo(message: types.Message):
+    # Получаем объект изображения
+    photo = message.photo[-1]
+    file_id = photo.file_id
+    file_path = f'/home/valery/Photo_coin/{file_id}.jpg'
+
+    # Сохраняем изображение на сервере
+    await photo.download(destination=file_path)
+
+    # Открываем файл и считываем его содержимое в бинарном виде
+    with open(file_path, 'rb') as f:
+        data = f.read()
+
+    # Записываем данные изображения в базу данных
+    cursor.execute("INSERT INTO photos (image) VALUES (%s)", (data,))
+    mydb.commit()
+
+    await message.reply("Изображение успешно сохранено!")
 
 
 # 8. Создаем команду `/remove_coin`, чтобы пользователи могли удалять монеты из своей коллекции:
@@ -193,10 +189,12 @@ async def on_help_command(message: types.Message):
     help_text = "список доступных команд:\n\n" \
                 "/start - запуск бота\n" \
                 "/help - справка\n" \
-                "/create_collection - создать коллекцию\n"\
+                "/create_collection - создать коллекцию\n" \
                 "/add_coin - добавить монету\n" \
                 "/remove_coin - удалить монету\n" \
+                "/add_photo - тест добавления монеты\n" \
                 "/show_collection - показать коллекцию\n"
+
     await message.answer(help_text)
 
 
